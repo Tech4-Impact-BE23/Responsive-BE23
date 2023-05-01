ed3348ba280f4c424ee01c31fb7397c81abac8ac
GINN30 <adyagi14@gmail.com>
2023-05-01 19:00:10 +0700

    undevelope header Adya

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     // your custom JavaScript code goes here
 });
 
-fetch('https://644d30d8cfdddac9709f3a9d.mockapi.io/AYF/icons')
-    .then(response => response.json())
-    .then(data => {
-        const avatarUrl = data[0].avatar;
-        const img = document.getElementById('avatar');
-        img.src = avatarUrl;
-    })
-    .catch(error => console.error(error));
+// fetch('https://644d30d8cfdddac9709f3a9d.mockapi.io/AYF/icons')
+//     .then(response => response.json())
+//     .then(data => {
+//         const avatarUrl = data[0].avatar;
+//         const img = document.getElementById('avatar');
+//         img.src = avatarUrl;
+//     })
+//     .catch(error => console.error(error));
