cd7842ed22f6143ac0a50af443d3d53f2b2b0da4
Widhiasywa Pranidya Icharussya <icharussya23@gmail.com>
2023-05-02 10:03:45 +0700

    page article

---
+++ article/article.js
@@ -0,0 +1,121 @@
+// javascript
+
+//filter array
+
+let filterArry = [];
+
+let galleryArray = [
+    {
+        id: 1,
+        name: "The Wall Street Journal",
+        author: "The Wall Street Journal",
+        title: "Lebrecht — One Man's Relationship With Beethoven's Music",
+        description: "It is a sad fact in the 2020s that anyone writing a book praising the achievements of an artist on the order of Ludwig van Beethoven situates himself on the dangerous side of a political question. The Wall Street Journal",
+        url: "https://www.wsj.com/articles/why-beethoven-review-can-a-genius-survive-a9e1e516",
+        urlToImage: "https://images.wsj.net/im-770110/social",
+        publishedAt: "2023-04-28T17:26:00Z",
+        src : "./images/1.jpg",
+        content: "In 2010 the British music critic Norman Lebrecht published Why Mahler? The book was an attempt to explain why the symphonies of Gustav Mahler (1860-1911) inspired both hatred and adoration in his lif… [+781 chars]",
+    },
+    {
+        id: 2,
+        name: "The Wall Street Journal",
+        author: "wsj",
+        title: "The Ugly World of Add-On Fees for Everything",
+        description: "Here's a trend I hope doesn't catch on: Paying extra for things I like less",
+        url: "https://www.wsj.com/articles/the-ugly-world-of-add-on-fees-for-everything-e41dc776",
+        urlToImage: "https://images.wsj.net/im-770864/social",
+        publishedAt: "2023-04-28T14:00:04Z",
+        src: "2.jpg",
+        content: "The Ugly World of Add-On Fees for Everything\r\nI couldn't watch the lowly Colorado Rockies get smacked around by my Philadelphia Phillies on Sunday; I forgot that the TV broadcast was only on Peacock.… [+1030 chars]",
+    },
+    {
+        id: 3,
+        name: "The Wall Street Journal",
+        author: "wsj",
+        title: "NOONAN: Biden vs. Trump in 2024? Don't Be So Sure...",
+        description: "Look at voters' faces when you describe the match-up and you'll realize they're open to alternatives.",
+        url: "https://www.wsj.com/articles/biden-vs-trump-dont-be-so-sure-rfk-third-party-rematch-2024-election-independents-bea741f2",
+        urlToImage: "https://images.wsj.net/im-771700/social",
+        publishedAt: "2023-04-28T12:00:04Z",
+        src: "3.jpg",
+        content: "Opinion | Biden vs. Trump in 2024? Don't Be So SureWonder Land: A beside-the-point president is the best thing that has ever happened to the progressive centralization project. But its success in 202… [+1333 chars]"
+    },
+    {
+        id: 4,
+        name: "The Wall Street Journal",
+        author: "wsj",
+        title: "USA Arms Warplanes With 'Bunker Busting' Bombs in Message to Iran...",
+        description: "The Air Force's A-10 Warthogs are carrying 250-pound precision-guided weapons in the Mideast",
+        url: "https://www.wsj.com/articles/u-s-arms-warplanes-with-bunker-busting-bombs-in-message-to-iran-ba1ecb08",
+        urlToImage: "https://images.wsj.net/im-771451/social",
+        publishedAt: "2023-04-28T12:00:04Z",
+        src: "4.jpg",
+        content: "WSJ News Exclusive | U.S. Arms Warplanes With 'Bunker Busting' Bombs in Message to Iran\r\nThe U.S. military is for the first time putting 250-pound “bunker busting” bombs on attack aircraft recently s… [+1076 chars]"
+    },
+    {
+        id: 5,
+        name: "The Wall Street Journal",
+        author: "wsj",
+        title: "'I Cloned Myself With AI. She Fooled My Bank and My Family'...",
+        description: "Our columnist replaced herself with AI voice and video to see how humanlike the tech can be. The results were eerie.",
+        url: "https://www.wsj.com/articles/i-cloned-myself-with-ai-she-fooled-my-bank-and-my-family-356bd1a3",
+        urlToImage: "https://images.wsj.net/im-771604/social",
+        publishedAt: "2023-04-28T11:00:04Z",
+        src: "5.jpg",
+        content: "I Cloned Myself With AI. She Fooled My Bank and My Family.Photo illustration: Elena ScottiThe good news about AI Joanna: She never loses her voice, she has outstanding posture and not even a converti… [+795 chars]"
+    },
+]
+
+//function to show gallery
+
+// showGallery(galleryArray);
+
+// function showGallery(currArray){
+
+//     for(var i=0; i<currArray.length; i++){
+//         document.querySelector("card").innerHTML += `
+//         <div class="col-md-4 mt-3">
+//         <div class="card p-3 ps-5 pe-5">
+//             <h4 class="text-capitalize text-center">${currArray[i].title}</h4>
+//             <img src="${currArray[i].src}" width="100%" height="320px"/>
+//             <p class="mt-2">${currArray[i].description}</p>
+//             <button class="btn btn-primary w-100 mx-auto">More</button>
+//         </div>
+//         </div>
+   
+//         `
+//     }
+
+// }
+
+
+const articleList = document.getElementById('articleList');
+// let hpCharacters = [];
+
+// const loadCharacters = async () => {
+//     try {
+//         const res = await fetch('https://hp-api.herokuapp.com/api/characters');
+//         hpCharacters = await res.json();
+//         displayCharacters(hpCharacters);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// };
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
